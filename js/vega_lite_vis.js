abdeb82ec2770351021d1526b644a62772f16be0
MaddieSavic <30588636+MaddieSavic@users.noreply.github.com>
2021-10-07 10:08:24 +1100

    initial commit

---
+++ js/vega_lite_vis.js
@@ -0,0 +1,14 @@
+var vg_1 = "js/choroplethmap.vg.json";
+vegaEmbed("#importMap", vg_1).then(function(result) {
+// Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
+}).catch(console.error);
+
+var vg_2 = "js/purpose.vg.json";
+vegaEmbed("#purposeBar", vg_2).then(function(result) {
+// Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
+}).catch(console.error);
+
+var vg_3 = "js/condition.vg.json";
+vegaEmbed("#conditionBar", vg_3).then(function(result) {
+// Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
+}).catch(console.error);
