c8e96d1fd1071da17bd5cd6be723ac49c58a3929
MaddieSavic <30588636+MaddieSavic@users.noreply.github.com>
2021-10-11 20:44:54 +1100

    added export map to index and editing sunburst chart

--- js/vega_lite_vis.js
+++ js/vega_lite_vis.js
@@ -3,12 +3,23 @@ vegaEmbed("#importMap", vg_1).then(function(result) {
 // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
 }).catch(console.error);
 
-var vg_2 = "js/purpose.vg.json";
-vegaEmbed("#purposeBar", vg_2).then(function(result) {
+var vg_2 = "js/exportMap.vg.json";
+vegaEmbed("#exportMap", vg_2).then(function(result) {
 // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
 }).catch(console.error);
 
-var vg_3 = "js/condition.vg.json";
-vegaEmbed("#conditionBar", vg_3).then(function(result) {
+var vg_3 = "js/purpose.vg.json";
+vegaEmbed("#purposeBar", vg_3).then(function(result) {
 // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
 }).catch(console.error);
+
+var vg_4 = "js/condition.vg.json";
+vegaEmbed("#conditionBar", vg_4).then(function(result) {
+// Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
+}).catch(console.error);
+
+var vg_5 = "js/classification.vg.json";
+vegaEmbed("#sunburst", vg_5).then(function(result) {
+// Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
+}).catch(console.error);
+
