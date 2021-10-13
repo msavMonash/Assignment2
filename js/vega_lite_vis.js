7d0510c5ec66b0f238d03c9b388f370c266e777b
MaddieSavic <30588636+MaddieSavic@users.noreply.github.com>
2021-10-14 08:51:22 +1100

    html styling changes and editing protection data

--- js/vega_lite_vis.js
+++ js/vega_lite_vis.js
@@ -1,30 +1,30 @@
 var vg_1 = "js/choroplethmap.vg.json";
-vegaEmbed("#importMap", vg_1).then(function(result) {
+vegaEmbed("#importMap", vg_1, {"actions": false}).then(function(result) {
 // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
 }).catch(console.error);
 
 var vg_2 = "js/exportMap.vg.json";
-vegaEmbed("#exportMap", vg_2).then(function(result) {
+vegaEmbed("#exportMap", vg_2, {"actions": false}).then(function(result) {
 // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
 }).catch(console.error);
 
 var vg_3 = "js/purpose.vg.json";
-vegaEmbed("#purposeBar", vg_3).then(function(result) {
+vegaEmbed("#purposeBar", vg_3, {"actions": false}).then(function(result) {
 // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
 }).catch(console.error);
 
 var vg_4 = "js/condition.vg.json";
-vegaEmbed("#conditionBar", vg_4).then(function(result) {
+vegaEmbed("#conditionBar", vg_4, {"actions": false}).then(function(result) {
 // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
 }).catch(console.error);
 
 var vg_5 = "js/classification.vg.json";
-vegaEmbed("#sunburst", vg_5).then(function(result) {
+vegaEmbed("#sunburst", vg_5, {"actions": false}).then(function(result) {
 // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
 }).catch(console.error);
 
 var vg_6 = "js/protection.vg.json";
-vegaEmbed("#protection", vg_6).then(function(result) {
+vegaEmbed("#protection", vg_6, {"actions": false}).then(function(result) {
 // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
 }).catch(console.error);
 
